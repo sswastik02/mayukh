f83bf03d2cfd320596f7b8c2fa594d667af1fdab
sswastik02 <sswastik02@gmail.com>
2021-05-02 15:01:38 +0530

    whitenoise fix

--- mayukh/settings.py
+++ mayukh/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'), #trailing comma is necessary
 ) # new
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
