511e165e178e9d33d10824dad1054d9c0bd65a16
sswastik02 <sswastik02@gmail.com>
2021-05-02 09:51:08 +0530

    Heroku commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mayukh.wsgi --log-file -
--- mayukh/settings.py
+++ mayukh/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-1x#m7mhfn6+!p08&yy^epnn-)2z=6+tz(9j4ac2!s^9#!_zyw1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'herokudjangoapp.herokuapp.com',
+]
 
 
 # Application definition
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -124,9 +129,19 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'), #trailing comma is necessary
 ) # new
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'  #self-explantory
 LOGOUT_REDIRECT_URL = 'home'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
+
+
+
