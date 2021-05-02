91031fcac5c51cb8d69dc1e0058ceae9386192d9
sswastik02 <sswastik02@gmail.com>
2021-05-02 13:45:29 +0530

    heroku deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mayukh.wsgi --log-file -
+web: gunicorn mayukh.wsgi
--- mayukh/wsgi.py
+++ mayukh/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import whitenoise.django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mayukh.settings')
 
 application = get_wsgi_application()
+application = Django
+
